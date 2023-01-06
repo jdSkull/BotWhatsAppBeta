ddd32223067249229ee05be95b2b04bda62d2b3f
JD Luiz <koitobacana@gmail.com>
2023-01-06 18:26:40 -0300

    Modificado menssagens para ser colocada

--- BotWhatsappClube/Mensagem.cs
+++ BotWhatsappClube/Mensagem.cs
@@ -8,9 +8,7 @@ namespace BotWhatsappClube
         public override string ToString()
         {
             StringBuilder sb = new StringBuilder();
-            sb.AppendLine("Olá, Somos o Clube da Sorte estou passando aqui pra te informar do nosso novo Sorteio!");
-            sb.AppendLine("*RESULTADO DIA 17/12/2022, COM APENAS UMA COTA DE 0,03 CENTAVOS VOCE PODE PARTICIPAR DO SORTEIO CONCORRENDO A R$ 1.500,00 E O RSULTADO SAIRA PELA LOTERIA FEDERAL*");
-            sb.AppendLine("Segue a gente no Instagram pra ficar por dentro de todas as novidades. *E VEJA TODOS OS NOSSOS GANHADORES*");
+            sb.AppendLine("Menssagem para ser colocada");
 
             return sb.ToString();
         }
